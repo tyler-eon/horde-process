04b17216deeb4b42eceaee93a627dd99ec24c0e2
Tyler Eon <tyler@eon.dev>
2024-02-23 15:29:30 -0800

    Add license and ex_doc to publish package to hex

--- mix.exs
+++ mix.exs
@@ -4,6 +4,12 @@ defmodule Horde.Process.MixProject do
   def project do
     [
       app: :horde_process,
+      description: """
+      A helper module for constructing and managing processes using the Horde distributed process library.
+      """,
+      package: package(),
+      source_url: "https://github.com/tyler-eon/horde-process",
+      homepage_url: "https://github.com/tyler-eon/horde-process",
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
@@ -21,8 +27,18 @@ defmodule Horde.Process.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+    ]
+  end
+
+  # For Hex packaging.
+  defp package do
+    [
+      links: %{
+        "GitHub" => "https://github.com/tyler-eon/horde-process",
+        "HexDocs" => "https://hexdocs.pm/horde_process"
+      },
+      licenses: ["MIT"],
     ]
   end
 end
